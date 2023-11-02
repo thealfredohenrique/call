412c9d380f2e99be52c7c5d414626936b7c772a9
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-11-01 22:15:53 -0300

    feat: Add availability form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,8 @@ import {
   TextInput,
 } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
+import { z } from 'zod'
+import { useForm, useFieldArray, Controller } from 'react-hook-form'
 import { Container, Header } from '../styles'
 import {
   IntervalBox,
@@ -15,8 +17,41 @@ import {
   IntervalItem,
   IntervalsContainer,
 } from './styles'
+import { getWeekDays } from '@/src/utils/get-week-days'
+
+const timeIntervalsFormSchema = z.object({})
 
 export default function TimeIntervals() {
+  const {
+    control,
+    handleSubmit,
+    register,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '09:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '09:00', endTime: '18:00' },
+      ],
+    },
+  })
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+  const weekDays = getWeekDays()
+  const intervals = watch('intervals')
+
+  async function handleSetTimeIntervals() {
+    console.log()
+  }
+
   return (
     <Container>
       <Header>
@@ -28,51 +63,46 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalsContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
+          {fields.map((field, index) => (
+            <IntervalItem key={field.id}>
+              <IntervalDay>
+                <Controller
+                  name={`intervals.${index}.enabled`}
+                  control={control}
+                  render={({ field }) => (
+                    <Checkbox
+                      onCheckedChange={(checked) => {
+                        field.onChange(checked === true)
+                      }}
+                      checked={field.value}
+                    />
+                  )}
+                />
+                <Text>{weekDays[field.weekDay]}</Text>
+              </IntervalDay>
 
-            <IntervalInputs>
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-            </IntervalInputs>
-          </IntervalItem>
-
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
-
-            <IntervalInputs>
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                crossOrigin="anonymous"
-              />
-            </IntervalInputs>
-          </IntervalItem>
+              <IntervalInputs>
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={intervals[index].enabled === false}
+                  crossOrigin="anonymous"
+                  {...register(`intervals.${index}.startTime`)}
+                />
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  disabled={intervals[index].enabled === false}
+                  crossOrigin="anonymous"
+                  {...register(`intervals.${index}.endTime`)}
+                />
+              </IntervalInputs>
+            </IntervalItem>
+          ))}
         </IntervalsContainer>
 
         <Button type="submit">
