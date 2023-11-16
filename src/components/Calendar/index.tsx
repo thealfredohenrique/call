655e4b1a7e75fcbccaec128868f9dcddcda0b9e3
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-11-15 21:59:06 -0300

    feat(calendar): Add query for unavailable days

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -16,6 +16,7 @@ import {
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarWeek {
@@ -52,7 +53,7 @@ export function Calendar({ selectedDate, onDateSelect }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -92,7 +93,8 @@ export function Calendar({ selectedDate, onDateSelect }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
