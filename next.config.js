5cf36413419e3c1ecd31ae2c451da7d17e03f066
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-11-22 23:51:42 -0300

    chore(config): Setup modularized imports

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/dist/ssr{{member}}',
+    },
+  },
   experimental: {
     optimizePackageImports: ['@phosphor-icons/react'],
   },
-  /* modularizeImports: {
-    '@phosphor-icons/react': {
-      transform: '@phosphor-icons/react/{{member}}',
-    },
-  }, */
 }
 
 module.exports = nextConfig
