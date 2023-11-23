6c5e5678a7c6cfa7536956197fc65caa1b224db4
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-11-22 23:45:20 -0300

    chore(config): Enable experimental optimizePackageImports in Next.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  modularizeImports: {
+  experimental: {
+    optimizePackageImports: ['@phosphor-icons/react'],
+  },
+  /* modularizeImports: {
     '@phosphor-icons/react': {
       transform: '@phosphor-icons/react/{{member}}',
     },
-  },
+  }, */
 }
 
 module.exports = nextConfig
